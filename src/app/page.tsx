6945ca775cc55b8d77097de86863fcb2756a4f32
Yash Solanki <yash.solanki@groovyweb.co>
2026-03-05 15:00:46 +0530

    refactor: temporarily hide Projects section
    - Commented out the Projects component in Home page
    - Retained other sections (Hero, About, Experience, Contact, Footer)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <Hero />
       <About />
       {/* <Skills /> */}
-      <Projects />
+      {/* <Projects /> */}
       <Experience />
       <Contact />
       <Footer />
