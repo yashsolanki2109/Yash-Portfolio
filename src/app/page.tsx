d2eea4b5ffe8a13961ea9f24658e495dd1d395fe
Yash Solanki <yashsolanki0921@gmail.com>
2026-03-07 23:01:13 +0530

    SCRUM-16 Uncomment the Exeperince Component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
-// import Experience from "@/components/Experience";
+import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
@@ -13,7 +13,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
-      {/* <Experience />  */}
+      <Experience /> 
       <Contact />
       <Footer />
     </main>
