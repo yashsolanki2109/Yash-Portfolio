794684ea01e040fb75d6c1f2921162b0f95a1768
Yash Solanki <yashsolanki0921@gmail.com>
2026-03-07 23:07:55 +0530

    SCRUM-17 Comment out the Skills Section temporyr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <main className="min-h-screen">
       <Hero />
       <About />
-      <Skills />
+      {/* <Skills /> */}
       <Projects />
       <Experience /> 
       <Contact />
