6e959ccfd1a95eea9ce7c7c40ddb9f3a19475436
Yash Solanki <yashsolanki0921@gmail.com>
2026-03-07 13:19:23 +0530

    SCRUM-9 FIXED BUGS AND ISSUE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <Hero />
       <About />
       <Skills />
-      {/* <Projects /> */}
-      <Experience />
+      <Projects />
+      {/* <Experience /> */}
       <Contact />
       <Footer />
     </main>
