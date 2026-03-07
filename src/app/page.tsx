ff64828a704cdd0473a5df6a39567b70abaa7502
Yash Solanki <yashsolanki0921@gmail.com>
2026-03-07 22:18:05 +0530

    FIXED: FIXED IMPORT BUGS AND ISSUE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
-import Experience from "@/components/Experience";
+// import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
