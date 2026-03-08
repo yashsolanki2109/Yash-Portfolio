0ca0b92290db1dcd84929ec200eada3af654baae
Yash Solanki <yashsolanki0921@gmail.com>
2026-03-08 16:34:06 +0530

    Add Header in HOme Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen">
-
+    <h1>Hello World</h1>
       <Hero />
       <About />
       {/* <Skills /> */}
